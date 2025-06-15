9a4df0079ea547435ad37991da69e19a64c0a73d
pookl <pookl@SAT>
2025-06-15 17:51:41 +0700

    Add project files.

---
+++ StoreProject/FrmProductShow.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace StoreProject
+{
+    partial class FrmProductShow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvAllProduct = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFrmProductCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(932, 81);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "บริหารจัดการข้อมูลสินค้า";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lvAllProduct
+            // 
+            this.lvAllProduct.HideSelection = false;
+            this.lvAllProduct.Location = new System.Drawing.Point(40, 100);
+            this.lvAllProduct.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.lvAllProduct.Name = "lvAllProduct";
+            this.lvAllProduct.Size = new System.Drawing.Size(866, 304);
+            this.lvAllProduct.TabIndex = 1;
+            this.lvAllProduct.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(539, 510);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(367, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "* เมื่อต้องการแก้ไขหรือลบข้อมูลสินค้า ให้ดับเบิ้ลคลิกที่รายการสินค้า";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnFrmProductCreate
+            // 
+            this.btnFrmProductCreate.Image = global::StoreProject.Properties.Resources.basket;
+            this.btnFrmProductCreate.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnFrmProductCreate.Location = new System.Drawing.Point(682, 416);
+            this.btnFrmProductCreate.Margin = new System.Windows.Forms.Padding(6);
+            this.btnFrmProductCreate.Name = "btnFrmProductCreate";
+            this.btnFrmProductCreate.Size = new System.Drawing.Size(224, 88);
+            this.btnFrmProductCreate.TabIndex = 2;
+            this.btnFrmProductCreate.Text = "เพิ่มข้อมูลสินค้า";
+            this.btnFrmProductCreate.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnFrmProductCreate.UseVisualStyleBackColor = true;
+            // 
+            // FrmProductShow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 643);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFrmProductCreate);
+            this.Controls.Add(this.lvAllProduct);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.Name = "FrmProductShow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "บริการจัดการข้อมูลสินค้า";
+            this.Load += new System.EventHandler(this.FrmProductShow_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lvAllProduct;
+        private System.Windows.Forms.Button btnFrmProductCreate;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ StoreProject/FrmProductShow.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoreProject
+{
+    public partial class FrmProductShow : Form
+    {
+        public FrmProductShow()
+        {
+            InitializeComponent();
+        }
+
+        // Method แปลง Binary เป็นรูป
+        private Image convertByteArrayToImage(byte[] byteArrayIn)
+        {
+            if (byteArrayIn == null || byteArrayIn.Length == 0)
+            {
+                return null;
+            }
+            try
+            {
+                using (MemoryStream ms = new MemoryStream(byteArrayIn))
+                {
+                    return Image.FromStream(ms);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                // อาจเกิดขึ้นถ้า byte array ไม่ใช่ข้อมูลรูปภาพที่ถูกต้อง
+                Console.WriteLine("Error converting byte array to image: " + ex.Message);
+                return null;
+            }
+        }
+
+        // Method ดึงข้อมูลทั้งหมดจาก Product_tb มาแสดงที่ listView
+        private void getAllProductTolv()
+        {
+            String connectionString = @"Server=SAT\SQLEXPRESS;Database=store_db;Trusted_Connection=True";
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    sqlConnection.Open();
+
+                    //การทำงานกับตารางในฐานข้อมูล (SELECT, INSERT, UPDATE, DELETE)
+                    //สร้างคำสั่ง Sql ดึงข้อมูลทั้งหมดในตาราง product_tb
+                    String strSQL = "SELECT proId, proName,proPrice,ProQuan, ProUnit , ProStatus, proImage From product_tb";
+
+                    using (SqlDataAdapter dataAdapter = new SqlDataAdapter(strSQL, sqlConnection)) 
+                    { 
+                        //เอาข้อมูลที่ได้จาก strSQL เป็นก้อนใน dataAdapter มาทำให้เป็นตารางโดยใส่ไว้ใน DataTable
+                        DataTable dataTable = new DataTable ();
+                        dataAdapter.Fill(dataTable);
+
+                        //ตั้งค่า ListView
+                        lvAllProduct.Items.Clear();
+                        lvAllProduct.Columns.Clear();
+                        lvAllProduct.FullRowSelect = true;
+                        lvAllProduct.View = View.Details;
+
+                        //ตั้งค่าการแสดงรูปใน View
+                        if(lvAllProduct.SmallImageList == null)
+                        {
+                            lvAllProduct.SmallImageList = new ImageList();
+                            lvAllProduct.SmallImageList.ImageSize = new Size (50,50);
+                            lvAllProduct.SmallImageList.ColorDepth = ColorDepth.Depth32Bit;
+                        }
+                        lvAllProduct.SmallImageList.Images.Clear();
+
+                        //กำหนดรายละเอียด Colum ใน ListView
+                        lvAllProduct.Columns.Add("รูปภาพ", 80, HorizontalAlignment.Left);
+                        lvAllProduct.Columns.Add("รหัสสินค้า", 80, HorizontalAlignment.Left);
+                        lvAllProduct.Columns.Add("ชื่อสินค้า", 150, HorizontalAlignment.Left);
+                        lvAllProduct.Columns.Add("ราคา", 80, HorizontalAlignment.Left);
+                        lvAllProduct.Columns.Add("จำนวน", 80, HorizontalAlignment.Left);
+                        lvAllProduct.Columns.Add("หน่วย", 80, HorizontalAlignment.Left);
+                        lvAllProduct.Columns.Add("สถานะ", 80, HorizontalAlignment.Left);
+
+
+                        //Loop วนเข้าไปใน Datatable 
+                        foreach(DataRow dataRow in dataTable.Rows)
+                        {
+                            ListViewItem item = new ListViewItem(); //สร้าง ITem เพื่อเก็บข้อมูลในแต่ละรายการ
+                            //เอารูปใส่ใน Item
+                            Image proImage = null;
+                            if (dataRow["proImage"]!= DBNull.Value)
+                            {
+                                byte[] imgByte = (byte[])dataRow["proImage"];
+                                //แปลงข้อมูลรูปจากฐานข้อมูล Binary ให้เป็นรูป
+                                proImage = convertByteArrayToImage(imgByte);
+                            }
+                            string imageKey = null;
+                            if (proImage != null)
+                            {
+                                imageKey = $"pro_{dataRow["proId"]}";
+                                lvAllProduct.SmallImageList.Images.Add(imageKey, proImage);
+                                item.ImageKey = imageKey;
+                            }else
+                            {
+                                item.ImageIndex = -1;
+                            }
+                            // เอาแต่ละรายการใส่ใน Item
+                            item.SubItems.Add(dataRow["proId"].ToString());
+                            item.SubItems.Add(dataRow["proName"].ToString());
+                            item.SubItems.Add(dataRow["proPrice"].ToString());
+                            item.SubItems.Add(dataRow["proQuan"].ToString());
+                            item.SubItems.Add(dataRow["proUnit"].ToString());
+                            item.SubItems.Add(dataRow["proStatus"].ToString());
+
+                            //เอาข้อมูลใน Item 
+                            lvAllProduct.Items.Add(item);
+                        }
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("พบข้อผิดพลาด กรุณาลองใหม่หรือติดต่อ IT : " + ex.Message);
+                }
+            }
+        }
+
+
+        private void FrmProductShow_Load(object sender, EventArgs e)
+        {
+            getAllProductTolv();
+        }
+    }
+}
